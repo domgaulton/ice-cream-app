0c4dff546d6f5cbfcccdd8681c625fd9fabed1fd
Dom Gaulton <d.gaulton@catchdigital.com>
2020-06-04 15:17:04 +0100

    Update cards and meta plus functionality

--- src/App.js
+++ src/App.js
@@ -4,30 +4,58 @@ import './App.css';
 
 function App() {
   const [list, createList] = useState([]);
+  const [filter, setFilter] = useState('')
 
   useEffect(() => {
     // Make a request for a user with a given ID
     axios
-      .get('https://ice-creams-api.herokuapp.com/')
+      .get(`https://ice-creams-api.herokuapp.com/${filter}`)
       .then(response => createList(response.data))
       .catch(error => console.log(error))
       .then(function () {
         // always executed
     });
-  }, []);
+  }, [filter]);
+
+  const updateFilter = e => {
+    const filterType = e.target.dataset.filterType
+    console.log(e.target.dataset.filterType)
+    setFilter(filterType)
+  }
 
   return (
     <div className="App">
-      {list.map(item => {
-        return (
-          <div className="ice-cream-card">
-            <h4>Name: {item.name}</h4>
-            <p>Rating: {item.rating}</p>
-            <img src={item.image} alt='alt'/>
-            <p>DG Says: {item.tasting_notes}</p>
-          </div>
-        )
-      })}
+      <header>
+        <h1>Quarnatine Ice Cream Challenge</h1>
+        <p><b>Total</b> {list.length}</p>
+        <button data-filter-type="" onClick={(e) => updateFilter(e)}>Reset</button>
+        <button data-filter-type="recent" onClick={(e) => updateFilter(e)}>Recent</button>
+        <button data-filter-type="worst" onClick={(e) => updateFilter(e)}>Worst</button>
+        <button data-filter-type="favourite" onClick={(e) => updateFilter(e)}>Favourite</button>
+      </header>
+      <main>
+        {list.length && list.map((item, index) => {
+
+          const date = item.date.split('T')[0]
+          return (
+            <div
+              key={`${item.name}-${index}`}
+              className="ice-cream-card"
+            >
+            <div className="ice-cream-card__image">
+              <img src={item.image} alt='alt'/>
+            </div>
+            <div className="ice-cream-card__text">
+              <h4><b>Name:</b> {item.name}</h4>
+              <p><b>Rating:</b> {item.rating}</p>
+              <p><b>DG Says:</b> {item.tasting_notes}</p>
+              <p className="meta"><b>Website</b> <a href="{item.url}">{item.name}</a></p>
+              <p className="meta"><b>Scoff Date:</b> {date}</p>
+            </div>
+            </div>
+          )
+        })}
+      </main>
     </div>
   );
 }
