f40ede529c683fa8d5d83f2ca9915b8f33e963a4
Dom Gaulton <d.gaulton@catchdigital.com>
2020-07-01 14:29:42 +0100

    Updates to cards

--- src/App.js
+++ src/App.js
@@ -3,24 +3,42 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [list, createList] = useState([]);
+  const [list, setList] = useState([]);
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    // Make a request for a user with a given ID
     axios
-      .get(`https://ice-creams-api.herokuapp.com/${filter}`)
-      .then(response => createList(response.data))
+      .get('https://ice-creams-api.herokuapp.com/')
+      .then(response => setList(response.data))
       .catch(error => console.log(error))
       .then(function () {
         // always executed
     });
-  }, [filter]);
+  }, []);
+
+  useEffect(() => {
+    const sortArray = key => {
+      let sort = key
+      if (key === 'reset') {
+        sort = 'date';
+      }
+      const sorted = [...list].sort((a, b) => b[sort] < a[sort] ? -1 : b[sort] > a[sort] ? 1 : 0);
+      if ( key === 'name' || key === 'reset' ) {
+        console.log('reset')
+        sorted.reverse();
+      }
+      setList(sorted);
+    };
+
+    sortArray(filter);
+
+  }, [filter, setList]);
+
+
 
   const updateFilter = e => {
-    const filterType = e.target.dataset.filterType
-    console.log(e.target.dataset.filterType)
-    setFilter(filterType)
+    const sortType = e.target.dataset.filterType
+    setFilter(sortType)
   }
 
   return (
@@ -28,10 +46,12 @@ function App() {
       <header>
         <h1>Quarnatine Ice Cream Challenge</h1>
         <p><b>Total</b> {list.length}</p>
-        <button data-filter-type="" onClick={(e) => updateFilter(e)}>Reset</button>
-        <button data-filter-type="recent" onClick={(e) => updateFilter(e)}>Recent</button>
-        <button data-filter-type="worst" onClick={(e) => updateFilter(e)}>Worst</button>
-        <button data-filter-type="favourite" onClick={(e) => updateFilter(e)}>Favourite</button>
+        <div className="header-buttons">
+          <button data-filter-type="reset" onClick={(e) => updateFilter(e)}>Reset</button>
+          <button data-filter-type="name" onClick={(e) => updateFilter(e)}>A-Z</button>
+          <button data-filter-type="date" onClick={(e) => updateFilter(e)}>Recent</button>
+          <button data-filter-type="rating" onClick={(e) => updateFilter(e)}>Favourite</button>
+        </div>
       </header>
       <main>
         {list.length && list.map((item, index) => {
@@ -51,6 +71,11 @@ function App() {
               <p><b>DG Says:</b> {item.tasting_notes}</p>
               <p className="meta"><b>Website</b> <a href={item.url}>{item.name}</a></p>
               <p className="meta"><b>Scoff Date:</b> {date}</p>
+              <div className="category">
+                {item.categories.map(category => {
+                  return <span>{category}</span>
+                })}
+              </div>
             </div>
             </div>
           )
